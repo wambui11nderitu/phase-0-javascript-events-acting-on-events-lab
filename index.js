46e41de5cfba56ecf3a22f04a12e64a3ed1253b2
Wambui <nderituwambui93@gmail.com>
2024-04-05 01:59:15 +0300

    event acting lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rock Dodger</title>
+</head>
+<body>
+    <div id="game" style="width: 400px; height: 400px; background-color: black;">
+        <div id="dodger" style="width: 40px; height: 20px; background-color: #FF69B4; position: absolute; bottom: 0; left: 180px;"></div>
+    </div>
+
+    <script src="index.js"></script>
+</body>
+</html>
